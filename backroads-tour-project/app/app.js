714ebe972e7e651169abe8dde57d12aeba4f8bd6
André <dev-asantos@outlook.com>
2020-05-20 16:57:41 -0400

    Finalizado a estrutura do javascript necessária para o projeto no arquivo app.js

---
+++ backroads-tour-project/app/app.js
@@ -0,0 +1,31 @@
+// SET DATA
+const date = (document.getElementById("date").innerHTML = new Date().getFullYear);
+
+// NAV TOGGLE
+const navBtn = document.getElementById("nav-toggle");
+const links = document.getElementById("nav-links");
+
+navBtn.addEventListener("click", () => {
+    links.classList.toggle("show-links");
+});
+
+// SMOOTH SCROLL
+const scrollLinks = document.querySelectorAll(".scroll-link");
+
+scrollLinks.forEach(link => {
+    link.addEventListener("click", e => {
+        e.preventDefault();
+
+        links.classList.remove("show-links");
+        
+        const id = e.target.getAttribute("href").slice(1);
+        const element = document.getElementById(id);
+        let position = element.offsetTop - 62;
+
+        window.scrollTo({
+            left: 0, 
+            top: position,
+            behavior: "smooth" 
+        });
+    });
+});
