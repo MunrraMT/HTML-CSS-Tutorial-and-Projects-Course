8edc1fab973107697475859159b8570b2fb0b049
André <dev-asantos@outlook.com>
2020-05-27 17:32:29 -0400

    Portfolio CSS Grid Project - Criado funcionalidades em javascript para o projeto no arquivo app/app.js

---
+++ portfolio-css-grid-project/app/app.js
@@ -0,0 +1,28 @@
+// variables
+const navbar = document.querySelector("#nav");
+const navBtn = document.querySelector("#nav-btn");
+const closeBtn = document.querySelector("close-btn");
+const sidebar = document.querySelector("sidebar");
+const date = document.querySelector("date");
+
+// add fixed class to navbar
+window.addEventListener("scroll", function() {
+    if(window.pageXOffset > 80) {
+        navbar.classList.add("navbar-fixed");
+    } else {
+        navbar.classList.remove("navbar-fixed");
+    }
+});
+
+// show sidebar
+navbar.addEventListener("click", function() {
+    sidebar.classList.add("show-sidebar");
+});
+
+// hide sidebar
+closeBtn.addEventListener("clicl", function() {
+    sidebar.classList.remove("show-sidebar");
+});
+
+// set years
+date.innerHTML = new Date().getFullYear();
