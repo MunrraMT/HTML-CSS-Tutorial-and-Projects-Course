4d3b85d456b9051bd3a5acd73d0148201d2d412e
André <dev-asantos@outlook.com>
2020-05-28 17:42:55 -0400

    Portfolio CSS Grid Project - SIDEBAR FUNCTIONALITY

--- portfolio-css-grid-project/app/app.js
+++ portfolio-css-grid-project/app/app.js
@@ -1,9 +1,9 @@
 // variables
 const navbar = document.querySelector("#nav");
 const navBtn = document.querySelector("#nav-btn");
-const closeBtn = document.querySelector("close-btn");
-const sidebar = document.querySelector("sidebar");
-const date = document.querySelector("date");
+const closeBtn = document.querySelector("#close-btn");
+const sidebar = document.querySelector("#sidebar");
+const date = document.querySelector("#date");
 
 // add fixed class to navbar
 window.addEventListener("scroll", function() {
@@ -15,12 +15,12 @@ window.addEventListener("scroll", function() {
 });
 
 // show sidebar
-navbar.addEventListener("click", function() {
+navBtn.addEventListener("click", function() {
     sidebar.classList.add("show-sidebar");
 });
 
 // hide sidebar
-closeBtn.addEventListener("clicl", function() {
+closeBtn.addEventListener("click", function() {
     sidebar.classList.remove("show-sidebar");
 });
 
